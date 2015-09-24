64bd63158e716be6515dc88eb256a81e10326c0a
megacartridge <mdltrack@gmail.com>
2015-09-24 02:52:44 -0400

    add WordMadness.rb

---
+++ WordMadness.rb
@@ -0,0 +1,10 @@
+puts "Give me a noun"
+noun1 = gets.chomp
+puts "Give me another noun"
+noun2 = gets.chomp
+puts "Give me a name"
+name = gets.chomp
+
+
+puts "Here's your story:"
+puts "In a " + noun1 + " in the " + noun2 + ", there lived a hobbit named " + name
